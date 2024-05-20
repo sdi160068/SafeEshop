5438c1deac7ff9a01264222e326ab9c9bae243d5
Christoforos Kladis <c.kladis@crowdpolicy.com>
2024-05-20 22:38:18 +0300

    adds RegisterForm

--- auth/forms.py
+++ auth/forms.py
@@ -3,3 +3,8 @@ from django import forms
 class LoginForm(forms.Form) :
     username = forms.CharField(label="username", max_length=100)
     password = forms.CharField(label="password", max_length=100)
+
+class RegisterForm(forms.Form) :
+    username = forms.CharField(label="username", max_length=100)
+    password = forms.CharField(label="password", max_length=100)
+    confirm_password = forms.CharField(label="password", max_length=100)
